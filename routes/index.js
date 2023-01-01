7955338cb9f7e1197809301b84e993dbda163021
ramanvellore2018@gmail.com <ramanvellore2018@gmail.com>
2023-01-01 17:58:40 +0530

    first commit

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('frontend/index', { title: 'Express' });
+  res.render('frontend/index', { title: 'Website Development Company in Vellore' });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('frontend/about', { title: 'Express' });
+  res.render('frontend/about', { title: 'Abour Ramsoft' });
 });
 
 router.get('/work', function(req, res, next) {
-  res.render('frontend/work', { title: 'Express' });
+  res.render('frontend/work', { title: 'Ram Work' });
 });
 
 router.get('/clients', function(req, res, next) {
-  res.render('frontend/clients', { title: 'Express' });
+  res.render('frontend/clients', { title: 'Clients details' });
 });
 
 router.get('/news', function(req, res, next) {
-  res.render('frontend/news', { title: 'Express' });
+  res.render('frontend/news', { title: 'News' });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('frontend/contact', { title: 'Express' });
+  res.render('frontend/contact', { title: 'Contact Us' });
 });
 module.exports = router;
 
